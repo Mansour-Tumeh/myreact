ed0cea8e3361fea4b0d149057f42a54fce2d4fce
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2019-11-03 21:37:03 +0100

    Updates

---
+++ precache-manifest.d292455ca87cf88a6896f6a419626cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc14229bd4fde63984e4de3de450464",
+    "url": "Mansour-Tumeh@github.io/myreact/index.html"
+  },
+  {
+    "revision": "dc40c4066268d9baa6d8",
+    "url": "Mansour-Tumeh@github.io/myreact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a53d116475475de8436d",
+    "url": "Mansour-Tumeh@github.io/myreact/static/js/2.89e95031.chunk.js"
+  },
+  {
+    "revision": "dc40c4066268d9baa6d8",
+    "url": "Mansour-Tumeh@github.io/myreact/static/js/main.963aa7e6.chunk.js"
+  },
+  {
+    "revision": "16265b3864716d690657",
+    "url": "Mansour-Tumeh@github.io/myreact/static/js/runtime-main.ae3f9d4d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "Mansour-Tumeh@github.io/myreact/static/media/logo.25bf045c.svg"
+  }
+]);
